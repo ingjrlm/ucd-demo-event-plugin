6305cba15377f705b55a15974bd3b50f2581e6ee
Jairo Molina <jairo.molina@hcl.com>
2025-06-26 22:20:32 -0600

    UCD Demo Event
    Initial version

---
+++ src/main/java/com/jrlm/ucd/demo/event/plugin/Util.java
@@ -0,0 +1,26 @@
+package com.jrlm.ucd.demo.event.plugin;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Util {
+    public static Properties readProperties(String propertiesFilePath)
+            throws IOException
+    {
+        Properties properties = new Properties();
+        try (FileInputStream inputStream = new FileInputStream(propertiesFilePath)) {
+            properties.load(inputStream);
+        }
+        return properties;
+    }
+
+    public static void writeProperties(String propertiesFilePath, Properties properties)
+            throws IOException
+    {
+        try (FileOutputStream outputStream = new FileOutputStream(propertiesFilePath)) {
+            properties.store(outputStream, "Output Properties");
+        }
+    }
+}
